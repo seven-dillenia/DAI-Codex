6ee3ce3949b8338dc68b468a6168ac5ae05a15a9
Lee Chu Yem <leechuyem@gmail.com>
2019-12-03 01:18:21 +1100

    edit whitespace

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -27,40 +27,41 @@ class _CategoryScreenState extends State<CategoryScreen> {
     return Scaffold(
       backgroundColor: Styles.black,
       body: SafeArea(
-          child: Container(
-        margin: EdgeInsets.symmetric(horizontal: Styles.smallSpacing),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverPersistentHeader(
-              delegate: CustomSliverAppBar(
-                expandedHeight: 200,
-                minHeight: 70,
-                hasBanner: true,
-                margin: EdgeInsets.only(left: 110, top: 20),
-                headerText: "Codex",
-                captionText: "Stories and background from the world of Thedas",
-                leading: Positioned(
-                  bottom: 0,
-                  child: Container(
-                    height: 200,
-                    child: Image.asset('assets/icons/banner.png'),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: Styles.smallSpacing),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverPersistentHeader(
+                delegate: CustomSliverAppBar(
+                  expandedHeight: 200,
+                  minHeight: 70,
+                  hasBanner: true,
+                  margin: EdgeInsets.only(left: 110, top: 20),
+                  headerText: "Codex",
+                  captionText: "Stories and background from the world of Thedas",
+                  leading: Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: 200,
+                      child: Image.asset('assets/icons/banner.png'),
+                    ),
                   ),
                 ),
+                pinned: true,
               ),
-              pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                SizedBox(height: Styles.bigSpacing),
-                Wrap(
-                  children: widget.categories.map((cat) => CategoryTile(text: cat)).toList(),
-                ),
-                SizedBox(height: Styles.bigSpacing)
-              ]),
-            )
-          ],
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  SizedBox(height: Styles.bigSpacing),
+                  Wrap(
+                    children: widget.categories.map((cat) => CategoryTile(text: cat)).toList(),
+                  ),
+                  SizedBox(height: Styles.bigSpacing)
+                ]),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
