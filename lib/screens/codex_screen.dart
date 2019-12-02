387d563b16c491cd5b2bd0974b057688c359c9af
Lee Chu Yem <leechuyem@gmail.com>
2019-12-03 01:18:09 +1100

    add appbar and sample text

--- lib/screens/codex_screen.dart
+++ lib/screens/codex_screen.dart
@@ -1,4 +1,7 @@
+import 'package:dai_codex/components/custom_sliver_app.dart';
+import 'package:dai_codex/misc/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CodexScreen extends StatefulWidget {
   static const id = "Codex Screen";
@@ -10,8 +13,42 @@ class _CodexScreenState extends State<CodexScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Styles.black,
       body: SafeArea(
-        child: Container(),
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverPersistentHeader(
+              delegate: CustomSliverAppBar(
+                expandedHeight: 70,
+                minHeight: 70,
+                headerText: "Sera",
+                leading: Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(
+                      FontAwesomeIcons.chevronLeft,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              pinned: false,
+              floating: true,
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                SizedBox(height: Styles.bigSpacing),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: Styles.smallSpacing),
+                  child: SelectableText("something", style: Styles.p,),
+                )
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
