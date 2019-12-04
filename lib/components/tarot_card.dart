ae78821233affb357b5f4d407accaffc9ea43415
Lee Chu Yem <leechuyem@gmail.com>
2019-12-05 01:19:23 +1100

    add shadow to tarot cards

--- lib/components/tarot_card.dart
+++ lib/components/tarot_card.dart
@@ -1,11 +1,10 @@
 import 'package:dai_codex/misc/styles.dart';
-import 'package:dai_codex/models/tarot_data.dart';
+import 'package:dai_codex/models/codex_data.dart';
 import 'package:dai_codex/screens/codex_screen.dart';
 import 'package:flutter/material.dart';
 
 class TarotCard extends StatefulWidget {
   final CodexData tarot;
-
   TarotCard({@required this.tarot});
 
   @override
@@ -27,26 +26,41 @@ class _TarotCardState extends State<TarotCard> {
         setState(() {
           this._isTapped = true;
         });
-
         Future.delayed(Duration(milliseconds: 500), () {
           setState(() {
             this._isTapped = false;
           });
-          Navigator.of(context).pushNamed(CodexScreen.id, arguments: widget.tarot.codexPath);
+          Navigator.of(context).pushNamed(CodexScreen.id, arguments: widget.tarot);
         });
       },
       child: AnimatedContainer(
         duration: Duration(milliseconds: 500),
         curve: Curves.easeInQuad,
-        width: 185,
         decoration: BoxDecoration(
-            border: Border.all(width: 2, color: this._isTapped ? Styles.shinyGold : Colors.transparent),
+          borderRadius: this._isTapped
+              ? BorderRadius.circular(8.0)
+              : BorderRadius.only(
+                  topLeft: Radius.circular(35), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(35)),
+          boxShadow: [
+            BoxShadow(
+                color: this._isTapped ? Styles.goldenRetriver : Color(0xff3F4239),
+                offset: this._isTapped ? Offset(2, 1) : new Offset(2, 3),
+                blurRadius: 5)
+          ],
+        ),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInQuad,
+          width: 185,
+          decoration: BoxDecoration(
+            border: Border.all(width: 2, color: this._isTapped ? Styles.goldenRetriver : Colors.transparent),
             borderRadius: BorderRadius.circular(8.0),
-            gradient: this._isTapped ? this.goldHue : this.transHue),
-        // color: Colors.red,
-        child: Opacity(
-          opacity: 0.8,
-          child: Image.asset(widget.tarot.tarotPath),
+            gradient: this._isTapped ? this.goldHue : this.transHue,
+          ),
+          child: Opacity(
+            opacity: 0.8,
+            child: Image.asset(widget.tarot.tarotPath),
+          ),
         ),
       ),
     );
