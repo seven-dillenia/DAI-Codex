4a264fb1175d83e730ea459480e53d92ff28a84f
Lee Chu Yem <leechuyem@gmail.com>
2019-12-05 01:19:07 +1100

    - implement start tag class - implement end tags class - add center tag - add h4 tag

--- lib/components/codex_text.dart
+++ lib/components/codex_text.dart
@@ -1,13 +1,19 @@
 import 'package:dai_codex/misc/styles.dart';
 import 'package:flutter/material.dart';
 
-// TODO: make enums for endtags and start tags
 class CodexText extends StatelessWidget {
   final List<String> lines;
-  final List<String> endTags = ["</i>", "</sera>", "</h3>", "</b>"];
-
   final Widget division = Divider(
     color: Styles.white,
+  );
+
+  final Widget center = Container(
+    width: double.infinity,
+    child: Center(
+        child: Text(
+      ". . .",
+      style: TextStyle(fontFamily: "Estre", color: Styles.white, fontSize: 32, height: 0),
+    )),
   );
 
   CodexText({@required this.lines});
@@ -17,37 +23,41 @@ class CodexText extends StatelessWidget {
     TextStyle _spanStyle = Styles.p;
 
     // Loop through each lines
-    // Start at 2 so the h1 and the <br> under it won't show up
-    for (int i = 2; i < lines.length; i++) {
+    for (int i = 0; i < lines.length; i++) {
       String oneLine = lines[i];
       List<String> words = oneLine.split(" ");
       List<TextSpan> spans = new List<TextSpan>();
       bool hasDivision = false;
+      bool hasCenter = false;
 
       // Loop through each word
       // Skip blank lines
       if (lines[i].isNotEmpty) {
         for (int j = 0; j < words.length; j++) {
           switch (words[j]) {
-            case "<i>":
+            case StartTags.italic:
               _spanStyle = Styles.i;
               break;
-            case "<h3>":
+            case StartTags.h3:
               _spanStyle = Styles.h3;
               break;
-            case "<b>":
-              _spanStyle = Styles.bold;
-              i++;
+            case StartTags.h4:
+              _spanStyle = Styles.boldItalicYellow;
               break;
-            case "<sera>":
+            case StartTags.bold:
+              _spanStyle = Styles.bold;
+              break;
+            case StartTags.sera:
               _spanStyle = Styles.sera;
               break;
-            case "<division>":
+            case StartTags.division:
               hasDivision = true;
               break;
-
+            case StartTags.center:
+              hasCenter = true;
+              break;
             default:
-              if (endTags.contains(words[j])) {
+              if (EndTags.allEndTags.contains(words[j])) {
                 _spanStyle = Styles.p;
               } else {
                 TextSpan s = TextSpan(text: words[j] + " ", style: _spanStyle);
@@ -67,6 +77,10 @@ class CodexText extends StatelessWidget {
       if (hasDivision) {
         widgets.add(this.division);
       }
+
+      if (hasCenter) {
+        widgets.add(center);
+      }
     }
 
     return widgets;
@@ -79,4 +93,33 @@ class CodexText extends StatelessWidget {
       children: buildLines(),
     );
   }
+}
+
+class StartTags {
+  static const italic = "<i>";
+  static const bold = "<b>";
+  static const h3 = "<h3>";
+  static const h4 = "<h4>";
+  static const center = "<center>";
+
+  // -------------------------- NOTE: Custom Tags
+  static const division = "<division>";
+
+  // -------------------------- NOTE: Character Tags
+  static const sera = "<sera>";
+}
+
+class EndTags {
+  static const itatlic = "</i>";
+  static const bold = "</b>";
+  static const h3 = "</h3>";
+  static const h4 = "</h4>";
+
+  // --------------------------- NOTE: Custom Tags
+
+  // --------------------------- NOTE: Character Tags
+  static const sera = "</sera>";
+
+  // --------------------------- NOTE: ALL
+  static const List<String> allEndTags = [itatlic, bold, h3, h4, sera];
 }
