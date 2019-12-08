246eee4727d272b4a3d4cc30439afe040a173710
Lee Chu Yem <leechuyem@gmail.com>
2019-12-08 23:37:10 +1100

    set colour for status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:dai_codex/misc/routes.dart';
+import 'package:dai_codex/misc/styles.dart';
 import 'package:dai_codex/screens/category_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as prefix0;
 
-void main() => runApp(DAICodex());
+// void main() => runApp(DAICodex());
+
+void main() {
+  runApp(DAICodex());
+  prefix0.SystemChrome.setSystemUIOverlayStyle(prefix0.SystemUiOverlayStyle(
+    statusBarColor: Styles.yellow,
+    statusBarBrightness: Brightness.dark
+  ));
+}
 
 
 class DAICodex extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+        // FlutterStatusbarcolor.setStatusBarColor(Colors.white);
     return MaterialApp(
+
       onGenerateRoute: RouteGenerator.generateRoute,
       home: CategoryScreen(),
     );
